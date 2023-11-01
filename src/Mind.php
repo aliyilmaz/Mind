4373f7096ad8e99217242b814b9a840b26550f32
Ali Yılmaz <aliyilmaz.work@gmail.com>
2023-11-01 17:37:50 +0300

    5.8.4

--- src/Mind.php
+++ src/Mind.php
@@ -3,7 +3,7 @@
 /**
  *
  * @package    Mind
- * @version    Release: 5.8.3
+ * @version    Release: 5.8.4
  * @license    GPL3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Php Framework, Design pattern builder for PHP.
