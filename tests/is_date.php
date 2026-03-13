acac599910016601d210b13a10e7e79a2e7f9d35
Ali Yılmaz <aliyilmaz.work@gmail.com>
2026-03-13 14:26:35 +0300

    Release: 6.1.5

--- tests/is_date.php
+++ tests/is_date.php
@@ -7,7 +7,7 @@ $time = '09:00'; // 09:00, 2026-01-03, 2026-01-03 14:22, 2026-01-03 14:22:22
 $format = 'H:i'; // H:i, Y-m-d, Y-m-d H:i, Y-m-d H:i:s
 
 if($Mind->is_date($time, $format)){
-    echo 'Geçerli';
+    echo 'Valid';
 } else {
-    echo 'Geçersiz';
+    echo 'Invalid';
 }
